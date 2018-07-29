7295a28d4803b80c87ff5ddcfd828d0679f63d62
Phil Kunz <gitlab@philkunz.com>
2018-07-29 18:56:36 +0200

    fix(core): upgrade to latest standards

--- test/test.ts
+++ test/test.ts
@@ -1,27 +1,25 @@
-import * as beautylog from 'beautylog';
-import 'should';
-import * as shelljs from 'shelljs';
-import { Qenv } from 'qenv';
-let testQenv = new Qenv(process.cwd(), process.cwd() + '/.nogit');
+import coretraffic = require("../ts/index");
 
-import coretraffic = require('../dist/index');
+import { tap, expect } from "@pushrocks/tapbundle";
+import { Qenv } from "qenv";
+let testQenv = new Qenv(process.cwd(), process.cwd() + "/.nogit");
 
-it(' .start()should start an coretraffic instance', function(done) {
+tap.test(" .start()should start an coretraffic instance", async (tools) => {
   this.timeout(240000);
-  coretraffic
-    .start({
-      cfKey: process.env.CF_KEY,
-      cfEmail: process.env.CF_EMAIL
-    })
-    .then(() => {
-      console.log('now starting a test container');
-      shelljs.exec(`docker run -e HOST=test1.bleu.de --name testContainer -d nginx`);
-      console.log('now starting a test handle change');
-      coretraffic.taskHandleChange.trigger().then(() => {
-        shelljs.exec(`docker stop testContainer`, { silent: true });
-        shelljs.exec(`docker rm testContainer`, { silent: true });
-        coretraffic.end();
-        done();
-      });
+  await coretraffic.start({
+    cfKey: process.env.CF_KEY,
+    cfEmail: process.env.CF_EMAIL
+  });
+  async () => {
+    console.log("now starting a test container");
+    shelljs.exec(
+      `docker run -e HOST=test1.bleu.de --name testContainer -d nginx`
+    );
+    console.log("now starting a test handle change");
+    coretraffic.taskHandleChange.trigger().then(() => {
+      shelljs.exec(`docker stop testContainer`, { silent: true });
+      shelljs.exec(`docker rm testContainer`, { silent: true });
+      coretraffic.end();
     });
+  };
 });
