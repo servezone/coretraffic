f270c112f951f56894413ab26bee5adcb988310b
Phil Kunz <gitlab@philkunz.com>
2019-01-10 19:23:41 +0100

    fix(core): update

--- ts/coretraffic.classes.coretraffic.ts
+++ ts/coretraffic.classes.coretraffic.ts
@@ -1,6 +1,6 @@
 import * as plugins from './coretraffic.plugins';
 import { logger } from './coretraffic.logging';
-import {} from './coretraffic.config';
+import { serviceQenv } from './coretraffic.config';
 import { SmartNginx } from '@pushrocks/smartnginx';
 import { DockerHost } from '@mojoio/docker';
 
@@ -8,12 +8,16 @@ export interface ICoretrafficConfig {
   dockerDomainEnvName?: string;
 }
 
-export class Coretraffic {
-  acmeRemoteClient: plugins.smartacme.CertRemoteClient;
-  dockerHost: plugins.docker.DockerHost;
-  smartNginx: SmartNginx;
+export class CoreTraffic {
+  private acmeRemoteClient: plugins.smartacme.CertRemoteClient;
+  private dockerHost: plugins.docker.DockerHost;
+  private smartNginx: SmartNginx;
 
-  public async init() {
+  constructor() {
+    this.acmeRemoteClient = new plugins.smartacme.CertRemoteClient({
+      remoteUrl: serviceQenv.getEnvVarOnDemand('SMARTACME_REMOTE_URL'),
+      secret: serviceQenv.getEnvVarOnDemand('SMARTACME_REMOTE_SECRET')
+    });
     this.dockerHost = new DockerHost(); // defaults to locally mounted docker sock
     this.smartNginx = new SmartNginx({ logger });
   }
@@ -25,13 +29,26 @@ export class Coretraffic {
     const eventObservable = await this.dockerHost.getEventObservable();
     const eventSubscription = eventObservable.subscribe(event => {
       logger.log('info', `Docker event`);
+      this.setupRoutingTask.trigger();
     });
-  };
+  }
+
+  /**
+   * a task to run setup routing, runs buffered
+   */
+  public setupRoutingTask = new plugins.taskbuffer.Task({
+    buffered: true,
+    bufferMax: 1,
+    taskFunction: async () => {
+      await this.setupRouting();
+    },
+    name: 'setupRouting',
+  });
 
   /**
    * sets up routing
    */
-  public async setupRouting () {
+  private async setupRouting () {
     const containers = await this.dockerHost.getContainers();
     this.smartNginx.wipeHosts(); // make sure we have a clean slate
     for (const container of containers) {
--- ts/index.ts
+++ ts/index.ts
@@ -2,39 +2,14 @@ console.log('**** Starting coretraffic ****');
 import * as plugins from './coretraffic.plugins';
 import * as paths from './coretraffic.paths';
 
-import * as CoretrafficConfig from './coretraffic.config';
-import * as CoretrafficDockersock from './coretraffic.dockersock';
-export * from './coretraffic.taskchains';
-import * as CoretrafficEnvironment from './coretraffic.environment';
-import * as CoretrafficEvents from './coretraffic.events';
-import * as CoretrafficNginx from './coretraffic.nginx';
+import {serviceQenv} from './coretraffic.config';
 
-/**************************************************************
- ************ Initial Start ********
- **************************************************************/
-let startDeferred = plugins.q.defer();
-let coreTrafficRunning = plugins.q.defer();
-startDeferred.promise.then(async () => {
-  await CoretrafficEnvironment.init();
-  await CoretrafficDockersock.init();
-  await CoretrafficNginx.init();
-  await CoretrafficEvents.init();
-});
+import { CoreTraffic } from './coretraffic.classes.coretraffic';
 
-/**
- * starts coretraffic with a given config;
- */
-export let start = (optionsArg: CoretrafficConfig.ICoretrafficConfig) => {
-  let done = plugins.q.defer();
-  CoretrafficConfig.setConfig(optionsArg);
-  startDeferred.resolve();
-  coreTrafficRunning.promise.then(() => {
-    plugins.beautylog.info('start tasks complete!');
-    done.resolve();
-  });
-  return done.promise;
-};
+const run = async () => {
+  const coretrafficInstance = new CoreTraffic();
+  coretrafficInstance.handleDockerEvents();
+  coretrafficInstance.setupRoutingTask.trigger();
+}
 
-export let end = () => {
-  CoretrafficDockersock.dockersock.endRequests();
-};
+
