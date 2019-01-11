c21509dfbaff4b8931aad371a8c907ee2bb614f3
Phil Kunz <github@philkunz.com>
2019-01-11 01:49:55 +0100

    fix(core): update

--- test/test.ts
+++ test/test.ts
@@ -2,8 +2,11 @@ import coretraffic = require('../ts/index');
 
 import { tap, expect } from '@pushrocks/tapbundle';
 import { Qenv } from '@pushrocks/qenv';
-let testQenv = new Qenv(process.cwd(), process.cwd() + '/.nogit');
+
+const testQenv = new Qenv('./', './.nogit');
 
 tap.test(' .start()should start an coretraffic instance', async (tools) => {
   
 });
+
+tap.start();
