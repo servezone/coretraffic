a67d89fcf2757ebfc796d221ce58f89fd56d6df3
Phil Kunz <gitlab@philkunz.com>
2019-09-19 16:36:45 +0200

    fix(core): update

--- test/test.ts
+++ test/test.ts
@@ -5,6 +5,8 @@ import { Qenv } from '@pushrocks/qenv';
 
 const testQenv = new Qenv('./', './.nogit');
 
-tap.test(' .start()should start an coretraffic instance', async tools => {});
+tap.test(' .start()should start an coretraffic instance', async tools => {
+  
+});
 
 tap.start();
