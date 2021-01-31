cd35217c54ae9a68f3d637573122abfdf6c80a15
Phil Kunz <gitlab@philkunz.com>
2021-01-31 19:53:12 +0000

    fix(core): update

--- ts/coretraffic.plugins.ts
+++ ts/coretraffic.plugins.ts
@@ -21,7 +21,6 @@ import * as smartdelay from '@pushrocks/smartdelay';
 import * as smartlog from '@pushrocks/smartlog';
 import * as smartlogDestinationReceiver from '@pushrocks/smartlog-destination-receiver';
 import * as smartshell from '@pushrocks/smartshell';
-import * as smartfile from '@pushrocks/smartfile';
 import * as smartproxy from '@pushrocks/smartproxy';
 import * as smartpromise from '@pushrocks/smartpromise';
 import * as smartrequest from '@pushrocks/smartrequest';
@@ -31,7 +30,6 @@ import * as taskbuffer from '@pushrocks/taskbuffer';
 export {
   qenv,
   projectinfo,
-  smartfile,
   smartdelay,
   smartlog,
   smartlogDestinationReceiver,
@@ -42,10 +40,3 @@ export {
   smartstring,
   taskbuffer,
 };
-
-// THIRD PARTY
-import * as moment from 'moment';
-import * as rxjs from 'rxjs';
-import * as through2 from 'through2';
-
-export { moment, rxjs, through2 };
